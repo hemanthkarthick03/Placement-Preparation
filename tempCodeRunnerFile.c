b4fcde10d5cada8971c08477b3e61f9b549414dd
phoenix.mp3 <hemanthkarthick03@gmail.com>
2023-12-01 21:58:07 +0530

    1 Dec 23

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int num1 ,num2;
+   int num3, num4;
+   scanf("%i%d",&num1 , &num2);
+   printf("%i\t%d\n",num1, num2);
+   num3 = 010;
+   num4 = 010;
+   printf("%i\t%d",num3, num4);
+   return 0;
+}
